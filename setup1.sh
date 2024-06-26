409c7df28c55c8b17761dffe698316aac004d9e1
Ken Keller <ken.keller@hashicorp.com>
2024-06-26 10:43:52 -0500

    initial commit

--- setup1.sh
+++ setup1.sh
@@ -1,34 +1,60 @@
 minikube start
+
+export VAULT_LICENSE="..."
+
+kubectl create ns vault
+
+kubectl create secret generic vault-license --from-literal license=$VAULT_LICENSE -n vault
+
 helm install vault hashicorp/vault -n vault --create-namespace --values vault/vault-values.yaml
+# vault-values updates for ent
 
 read -p "check Vault is ready then press enter to continue"
 
-kubectl port-forward -n vault statefulset/vault 8200:8200 &
-export VAULT_TOKEN=root  
-export VAULT_ADDR=http://127.0.0.1:8200
-vault status
+kubectl exec --stdin=true --tty=true vault-0 -n vault -- /bin/sh
 
-read -p "check ports forwaring  is ready then press enter to continue"
+vault namespace create us-west-org
 
-vault auth enable -path demo-auth-mount kubernetes
+VAULT_NAMESPACE=us-west-org vault auth enable -path demo-auth-mount kubernetes
 
-vault write auth/demo-auth-mount/config \
-   kubernetes_host="https://$KUBERNETES_PORT_443_TCP_ADDR:443"
+VAULT_NAMESPACE=us-west-org vault write auth/demo-auth-mount/config \
+	kubernetes_host="http://$KUBERNETES_PORT_443_TCP_ADDR:443"
 
-vault secrets enable -path=kvv2 kv-v2
+VAULT_NAMESPACE=us-west-org vault secrets enable -path=kvv2 kv-v2
+## could not run below in shell on vault server, had to remotely run
+VAULT_NAMESPACE=us-west-org vault policy write dev - <<EOF
+	path "kvv2/data/webapp/config" {
+	   capabilities = ["read", "list", "subscribe"]
+	   subscribe_event_types = ["kv*"]
+	}
 
-vault policy write dev - <<EOF
-path "kvv2/*" {
-   capabilities = ["read"]
-}
+	path "sys/events/subscribe/kv*" {
+   		capabilities = ["read"]
+	}
 EOF
-
-vault write auth/demo-auth-mount/role/role1 \
+# could not run remotely
+VAULT_NAMESPACE=us-west-org vault write auth/demo-auth-mount/role/role1 \
    bound_service_account_names=instant-update-app \
    bound_service_account_namespaces=app \
    policies=dev \
-   audience=vault \
-   ttl=24h
+   token_period=2m
 
-vault kv put kvv2/webapp/config username="static-user" password="static-password"
+# VAULT_NAMESPACE=us-west-org vault write auth/demo-auth-mount/role/role1 \
+#    bound_service_account_names=instant-update-app \
+#    bound_service_account_namespaces=app \
+#    policies=dev \
+#    audience=vault \
+#    ttl=24h
+
+VAULT_NAMESPACE=us-west-org vault kv put kvv2/webapp/config username="static-user" password="static-password"
+# demo uses kustomize instead of helm
+helm install vault-secrets-operator hashicorp/vault-secrets-operator -n vault-secrets-operator-system --create-namespace --values vault/vault-operator-values.yaml
+# below is test version with instant updates
+# helm install vault-secrets-operator ./chart --version 0.0.0-dev -n vault-secrets-operator-system --create-namespace --values vault/vault-operator-values.yaml
+
+kubectl create ns app
+
+kubectl apply -f vault/vault-auth-static.yaml
+
+kubectl apply -f vault/static-secret.yaml
 
